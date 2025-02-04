6199962e130de8150bc240580dac1d78a8291ec3
Martin Rist <mlr-github@martinrist.com>
2025-02-04 10:30:01 +0000

    Convert ProductCreateComponent to use FormGroup

--- books/learning-angular-5e/10-collecting-user-data-with-forms/my-app/src/app/product-create/product-create.component.ts
+++ books/learning-angular-5e/10-collecting-user-data-with-forms/my-app/src/app/product-create/product-create.component.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
 import {ProductsService} from '../products.service';
 import {Router} from '@angular/router';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-product-create',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './product-create.component.html',
   styleUrl: './product-create.component.css'
 })
 export class ProductCreateComponent {
+
+  productForm  = new FormGroup({
+    title: new FormControl('', { nonNullable: true }),
+    price: new FormControl<number | undefined>(undefined, { nonNullable: true }),
+    category: new FormControl('', { nonNullable: true }),
+  });
+
   constructor(private productService: ProductsService, private router: Router) {}
 
-  createProduct(title: string, price: string, category: string) {
-    this.productService.addProduct({
-      title,
-      price: Number(price),
-      category
-    }).subscribe(() => this.router.navigate(['/products']));
+  createProduct() {
+    this.productService.addProduct(this.productForm.value)
+      .subscribe(() => this.router.navigate(['/products']));
   }
 }

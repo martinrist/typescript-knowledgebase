526ac3d60e04628e2fa2edff680237def63e37af
Martin Rist <mlr-github@martinrist.com>
2025-02-04 10:54:08 +0000

    Use FormBuilder for product-create form

--- books/learning-angular-5e/10-collecting-user-data-with-forms/my-app/src/app/product-create/product-create.component.ts
+++ books/learning-angular-5e/10-collecting-user-data-with-forms/my-app/src/app/product-create/product-create.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProductsService} from '../products.service';
 import {Router} from '@angular/router';
-import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-product-create',
@@ -9,22 +9,34 @@ import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
   templateUrl: './product-create.component.html',
   styleUrl: './product-create.component.css'
 })
-export class ProductCreateComponent {
+export class ProductCreateComponent implements OnInit {
 
-  productForm  = new FormGroup({
-    title: new FormControl('', { nonNullable: true }),
-    price: new FormControl<number | undefined>(undefined, { nonNullable: true }),
-    category: new FormControl('', { nonNullable: true }),
-    extra: new FormGroup({
-      image: new FormControl(''),
-      description: new FormControl('')
-    })
-  });
+  productForm: FormGroup<{
+    title: FormControl<string>,
+    price: FormControl<number | undefined>,
+    category: FormControl<string>
+  }> | undefined;
 
-  constructor(private productService: ProductsService, private router: Router) {}
+  constructor(
+    private productsService: ProductsService,
+    private router: Router,
+    private builder: FormBuilder
+  ) {}
 
   createProduct() {
-    this.productService.addProduct(this.productForm.value)
+    this.productsService.addProduct(this.productForm!.value)
       .subscribe(() => this.router.navigate(['/products']));
+  }
+
+  private buildForm() {
+    this.productForm = this.builder.nonNullable.group({
+      title: [''],
+      price: this.builder.nonNullable.control<number | undefined>(undefined),
+      category: ['']
+    });
+  }
+
+  ngOnInit() {
+    this.buildForm();
   }
 }

d7fa15627db25edc96a23083d3376178ab33c169
Martin Rist <mlr-github@martinrist.com>
2025-02-06 12:15:39 +0000

    Complete 'Secret Service' part 2

--- books/learning-typescript/05-functions/exercises/secret-secrets/02-dr-on/index.ts
+++ books/learning-typescript/05-functions/exercises/secret-secrets/02-dr-on/index.ts
@@ -1,2 +1,28 @@
 // Write your createAdvancedCipher function here! ✨
 // You'll need to export it so the tests can run it.
+
+type Cipher = (character: string) => string;
+
+export function createAdvancedCipher(
+  onVowel: Cipher,
+  onConsonant: Cipher,
+  onPunctuation: Cipher
+) {
+
+  return (text: string) => {
+    let result = '';
+
+    for (const char of text) {
+      result +=
+        /[aeiou]/i.test(char)
+          ? onVowel(char)
+          : /[bcdfghjklmnpqrstvwxyz]/i.test(char)
+            ? onConsonant(char)
+            : onPunctuation(char);
+    }
+
+    return result;
+
+  };
+
+}

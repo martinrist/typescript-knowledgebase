8d129808459473edb266d58fcbc02f3fbbf5a382
Martin Rist <mlr-github@martinrist.com>
2025-02-14 15:20:25 +0000

    Create product-list component

--- tutorials/angular-15-getting-started/src/app/product-list/product-list.component.ts
+++ tutorials/angular-15-getting-started/src/app/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { products } from '../../../../../../../../Downloads/f5phy5tg--run/src/app/products';
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-list',
@@ -14,10 +14,3 @@ export class ProductListComponent {
     window.alert('The product has been shared!');
   }
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/

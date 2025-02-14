ef16a4be38104f89b924227697c708e4161b6364
Martin Rist <mlr-github@martinrist.com>
2025-02-14 15:52:56 +0000

    Add "Buy" button and integrate add-to-cart functionality
    This commit introduces a "Buy" button to the product details view, allowing users to add products to their cart. The functionality integrates with the CartService and provides a confirmation alert upon adding a product.

--- tutorials/angular-15-getting-started/src/app/product-details/product-details.component.ts
+++ tutorials/angular-15-getting-started/src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Product, products} from "../products";
+import {CartService} from "../cart.service";
 
 @Component({
   selector: 'app-product-details',
@@ -11,7 +12,10 @@ export class ProductDetailsComponent implements OnInit {
 
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
     // first get the product id from the current route
@@ -20,5 +24,10 @@ export class ProductDetailsComponent implements OnInit {
 
     // Find the product that corresponds to the id provided in the route
     this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCard(product);
+    window.alert('Your product has been added to your cart!');
   }
 }

5066533d32a4f7ea7fb7a2aad109877a01d60bab
Martin Rist <mlr-github@martinrist.com>
2025-02-14 15:41:03 +0000

    Complete product-details component

--- tutorials/angular-15-getting-started/src/app/product-details/product-details.component.ts
+++ tutorials/angular-15-getting-started/src/app/product-details/product-details.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Product, products} from "../products";
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
 
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    // first get the product id from the current route
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // Find the product that corresponds to the id provided in the route
+    this.product = products.find(product => product.id === productIdFromRoute);
+  }
 }

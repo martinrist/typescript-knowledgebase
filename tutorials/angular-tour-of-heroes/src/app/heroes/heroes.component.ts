79bfa7ba9b5db562d843841f9d19bada064378e0
Martin Rist <mlr-github@martinrist.com>
2025-02-14 18:28:03 +0000

    Refactor HeroesComponent to display a list of selectable heroes
    Replaced single hero detail view with a list of heroes and added a selection feature. Updated HTML and CSS to support the new UI, introducing styles for hero buttons and selection states. Integrated mock hero data and implemented a method to handle hero selection.
    
    See: https://v15.angular.io/tutorial/tour-of-heroes/toh-pt2

--- tutorials/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tutorials/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {Hero} from "../hero";
+import {HEROES} from "../mock-hereos";
 
 @Component({
   selector: 'app-heroes',
@@ -7,8 +8,11 @@ import {Hero} from "../hero";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+
+  selectedHero?: Hero;
+  heroes = HEROES;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }

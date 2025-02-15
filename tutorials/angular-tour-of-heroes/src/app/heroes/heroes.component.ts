ab4733fdbdbb1779b6b92192311f0230fe595153
Martin Rist <mlr-github@martinrist.com>
2025-02-15 11:33:46 +0000

    Add messaging functionality and integrate with heroes feature
    Introduced `MessageService` to manage app messages and display them in the new `MessagesComponent`. Updated `HeroesComponent` to use `HeroService` for fetching heroes and log interactions via `MessageService`. Integrated `MessagesComponent` into the application layout for enhanced user feedback.
    
    See: https://v15.angular.io/tutorial/tour-of-heroes/toh-pt4

--- tutorials/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tutorials/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,18 +1,34 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Hero} from "../hero";
-import {HEROES} from "../mock-hereos";
+import {HeroService} from "../hero.service";
+import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero;
-  heroes = HEROES;
+  heroes: Hero[] = [];
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: selected hero id=${hero.id}`);
   }
 }

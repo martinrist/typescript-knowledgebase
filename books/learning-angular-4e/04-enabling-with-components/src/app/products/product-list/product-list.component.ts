6e046082d7738cfffb1a068add7b195e623e8176
Martin Rist <mlr-github@martinrist.com>
2025-02-17 11:12:41 +0000

    Complete examples from Learning Angular 4e - 'Components'

---
+++ books/learning-angular-4e/04-enabling-with-components/src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,24 @@
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {ProductDetailComponent} from "../product-detail/product-detail.component";
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements AfterViewInit {
+  @ViewChild(ProductDetailComponent) productDetail!: ProductDetailComponent;
+  selectedProduct = 'foo';
+
+  ngAfterViewInit(): void {
+    if (this.productDetail) {
+      console.log(`ProductListComponent#ngAfterViewInit() - ${this.productDetail.name}`);
+    } else {
+      console.log(`ProductListComponent#ngAfterViewInit()`);
+    }
+  }
+
+  onBuy(name: string) {
+    window.alert(`You just bought ${name}`)
+  }
+}

33fd3741c506a471401fdadaf8d46f6244ea01f3
Martin Rist <mlr-github@martinrist.com>
2025-02-18 15:21:43 +0000

    Complete chapter 6 example app

--- books/learning-angular-4e/06-services-and-di/src/app/app.component.ts
+++ books/learning-angular-4e/06-services-and-di/src/app/app.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {ProductsService} from "./products/products.service";
+import {APP_CONFIG, AppConfig, appSettings} from "./app.config";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    { provide: APP_CONFIG, useValue: appSettings },
+  ]
 })
 export class AppComponent {
 
-  constructor(private productService: ProductsService) {
+  constructor(
+    private productService: ProductsService,
+    @Inject(APP_CONFIG) config: AppConfig
+    ) {
     // This will be the version of `ProductService` from the root injector
     console.log(`AppComponent: Using ProductService #${productService.serviceId}`);
   }
 
-  title = 'my-app';
+  get title(): string {
+    return appSettings.title
+  }
 }
--- books/learning-angular-4e/06-services-and-di/src/app/products/favorites/favorites.component.ts
+++ books/learning-angular-4e/06-services-and-di/src/app/products/favorites/favorites.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Host, OnInit, Optional} from '@angular/core';
 import {Product} from '../product';
 import {ProductsService} from "../products.service";
 

35564e6866645131e970fdf18bbbdfcc1ee97fb1
Martin Rist <mlr-github@martinrist.com>
2025-02-24 09:51:43 +0000

    Tidy up tsconfig.json and package.json

--- sandbox/src/index.ts
+++ sandbox/src/index.ts
@@ -1,3 +1,5 @@
-import {log} from './logger';
+// Note: Since `tsconfig.json#moduleResolution` is `nodeNext` and `package.json#type`
+// is `module`, then relative imports need to specify `.js`
+import {log} from './logger.js';
 
 log('Hello World!');

6dd5b41d85778d70b47e83a8087fa116233f3f98
Martin Rist <mlr-github@martinrist.com>
2025-02-24 10:18:03 +0000

    Add prettier and set up basic configuration

--- sandbox/README.md
+++ sandbox/README.md
@@ -2,7 +2,6 @@
 
 ## TODO
 
-- [ ] Set up prettier
 - [ ] Set up ESlint and TSLint
 - [ ] Set up `npm` scripts
 - [ ] Set up testing
--- sandbox/src/index.ts
+++ sandbox/src/index.ts
@@ -1,5 +1,5 @@
 // Note: Since `tsconfig.json#moduleResolution` is `nodeNext` and `package.json#type`
 // is `module`, then relative imports need to specify `.js`
-import {log} from './logger.js';
+import { log } from './logger.js';
 
 log('Hello World!');

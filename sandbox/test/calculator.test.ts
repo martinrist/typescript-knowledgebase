6ab13fe1be6f28e271f6bbaa0271eb2fcc7b4213
Martin Rist <mlr-github@martinrist.com>
2025-03-18 16:06:45 +0000

    Set up Mocha testing

--- sandbox/test/calculator.test.ts
+++ sandbox/test/calculator.test.ts
@@ -1,22 +1,23 @@
-import { describe, expect, it } from 'vitest';
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
 import { add, subtract } from '../src/calculator.js';
 
 describe('addition', () => {
   it('should add two positive numbers', () => {
-    expect(add(1, 1)).toBe(2);
+    expect(add(1, 1)).to.equal(2);
   });
 
   it('should add one positive and one negative number', () => {
-    expect(add(1, -1)).toBe(0);
+    expect(add(1, -1)).to.equal(0);
   });
 });
 
 describe('subtraction', () => {
   it('should subtract one positive number from another', () => {
-    expect(subtract(2, 1)).toBe(1);
+    expect(subtract(2, 1)).to.equal(1);
   });
 
   it('should subtract one negative number from a positive number', () => {
-    expect(subtract(1, -1)).toBe(2);
+    expect(subtract(1, -1)).to.equal(2);
   });
 });
